0991af3e1a52336f7f019ec68be35896522cdfad
Christopher Sams <csams@redhat.com>
2018-03-25 10:47:19 -0500

    Add ruby script to test driver.

--- test_script.py
+++ test_script.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 from insights import run
+from scripts.ruby_script import my_ruby
 from scripts.script1 import my_test
 from scripts.nested.script2 import report
 
-run([my_test, report], print_summary=True)
+run([my_ruby, my_test, report], print_summary=True)
