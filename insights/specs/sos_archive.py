e708ab34db35b60c9347487740a585b140b8cf6b
Robin Cernin <rcerninr@redhat.com>
2019-07-08 14:32:58 +1000

    Adding Ceph Health status for SOS spec
    Signed-off-by: Robin Cernin <rcerninr@redhat.com>

--- insights/specs/sos_archive.py
+++ insights/specs/sos_archive.py
@@ -23,6 +23,7 @@ class SosSpecs(Specs):
     catalina_server_log = glob_file("var/log/tomcat*/catalina*.log")
     ceph_osd_tree_text = simple_file("sos_commands/ceph/ceph_osd_tree")
     ceph_report = simple_file("sos_commands/ceph/ceph_report")
+    ceph_health = simple_file("/sos_commands/ceph/ceph_health_detail")
     chkconfig = simple_file("sos_commands/startup/chkconfig_--list")
     date = first_of([simple_file("sos_commands/general/date"), simple_file("sos_commands/date/date")])
     df__al = first_file(["sos_commands/filesys/df_-al", "sos_commands/filesys/df_-al_-x_autofs"])
